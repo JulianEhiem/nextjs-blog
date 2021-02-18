63dea3e456ddd5e1785ecd2c98e9968c782728d4
Julian Ehiem <jules.ehiem@gmail.com>
2021-02-17 21:36:04 -0500

    cleaned up index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-// import Head from 'next/head'
-// import BlogPage from '../components/BlogPage'
-// import styles from '../styles/Home.modules.css'
 import MyApp from './_app'
 
 export default function Home() {
