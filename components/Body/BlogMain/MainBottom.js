6abc8f782adb012146f90d86ec7b61cf7cee5c3b
Julian_Ehiem <jules.ehiem@gmail.com>
2021-02-22 09:55:29 -0500

    finished preliminary styling and responsive layout

--- components/Body/BlogMain/MainBottom.js
+++ components/Body/BlogMain/MainBottom.js
@@ -10,6 +10,31 @@ export default function MainBottom() {
       <div className={styles.MbBlogHeadDiv}>
         <h1>Blogpost #3 - MB</h1>
       </div>
+      <div className={styles.MbBlogBodyDiv}>
+        <p>
+          You can import a file right in a JavaScript module. This tells webpack
+          to s simply dummy text of the printing and typesetting industry. a
+          type specimen book. It has survived not only five centuries...
+          <br></br>
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Bonorum et Malorum" (The Extremes of
+          Good and Evil) by
+          You can import a file right in a JavaScript module. This tells webpack
+          to s simply dummy text of the printing and typesetting industry. a
+          type specimen book. It has survived not only five centuries...
+          <br></br>
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Bonorum et Malorum" (The Extremes of
+          Good and Evil) by
+          You can import a file right in a JavaScript module. This tells webpack
+          to s simply dummy text of the printing and typesetting industry. a
+          type specimen book. It has survived not only five centuries...
+          <br></br>
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Bonorum et Malorum" (The Extremes of
+          Good and Evil) by
+        </p>
+      </div>
     </div>
   );
 }
--- components/Body/BlogMain/MainTop.js
+++ components/Body/BlogMain/MainTop.js
@@ -10,6 +10,31 @@ export default function MainTop() {
       <div className={styles.MtBlogHeadDiv}>
         <h1>Blogpost #2 - MT</h1>
       </div>
+      <div className={styles.MtBlogBodyDiv}>
+        <p>
+          You can import a file right in a JavaScript module. This tells webpack
+          to s simply dummy text of the printing and typesetting industry. a
+          type specimen book. It has survived not only five centuries...
+          <br></br>
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Bonorum et Malorum" (The Extremes of
+          Good and Evil) by
+          You can import a file right in a JavaScript module. This tells webpack
+          to s simply dummy text of the printing and typesetting industry. a
+          type specimen book. It has survived not only five centuries...
+          <br></br>
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Bonorum et Malorum" (The Extremes of
+          Good and Evil) by
+          You can import a file right in a JavaScript module. This tells webpack
+          to s simply dummy text of the printing and typesetting industry. a
+          type specimen book. It has survived not only five centuries...
+          <br></br>
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Bonorum et Malorum" (The Extremes of
+          Good and Evil) by
+        </p>
+      </div>
     </div>
   );
 }
