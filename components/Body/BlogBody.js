5c764bdf7e7256bd59d8f2f8161ea29bdb658cf9
Julian Ehiem <jules.ehiem@gmail.com>
2021-02-24 07:22:44 -0500

    added older posts container

--- components/Body/BlogBody.js
+++ components/Body/BlogBody.js
@@ -2,13 +2,48 @@ import styles from "./blogBody.module.css";
 import BlogRight from "./BlogRight/BlogRight";
 import BlogLeft from "./BlogLeft/BlogLeft";
 import BlogMain from "./BlogMain/BlogMain";
+import { useContext } from "react";
+import { PostContext } from "../PostContext";
+import BlockContent from '@sanity/block-content-to-react';
+// import dummyFallBack from '../../DummyFallBack'
 
 export default function BlogBody() {
+
+  const posts = useContext(PostContext)
+  const post = posts.mappedPosts
+
+  // const title = post.length === 0 ? "Loading" : post[0].title;
+  // const image = post.length === 0 ? "Loading" : post[0].mainImage;
+  // const body = post.length === 0 ? dummyFallBack : post[0].body;
+
   return (
-    <div className={styles.blogBodyContainer}>
-      <BlogMain />
-      <BlogLeft />
-      <BlogRight />
-    </div>
+    <>
+      <div className={styles.blogBodyContainer}>
+        <BlogMain />
+        <BlogLeft />
+        <BlogRight />
+      </div>
+      <div className={styles.additionalPosts}>
+        {post.length < 7 ? <h2>Nothing here</h2> : 
+        post.slice(6).map(p => {
+          console.log(p);
+          <div className={styles.additionalPostsContainer}>
+            <div className={styles.additionalPostsImage}>
+              <img src={p.mainImage} width="100%"/>
+            </div>
+            <div className={styles.additionalPostsTitle}>
+              <h1>{p.title}</h1>
+            </div>
+            <div className={styles.additionalPostsBody}>
+              <p>
+              <BlockContent blocks={p.body}/>
+              </p>
+            </div>
+          </div>
+        })
+         }
+         hello
+      </div>
+    </>
   );
 }
