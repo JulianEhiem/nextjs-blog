c8dab5a38c30eae3c0d130951194cc99e6c9fdfe
JulianEhiem <jules.ehiem@gmail.com>
2022-09-09 16:39:27 -0400

    started working on Nav bar

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,14 +1,22 @@
 import styles from "./nav.module.css";
 import { useRouter } from 'next/router'
+import {AppBar, Container, Toolbar } from "@mui/material";
 
 export default function Nav() {
 const router = useRouter();
 
   return (
-    <div className={styles.navContainer}>
-      <div onClick={() => router.push('/')}> <h3>Home</h3> </div>
-      <div target ="_blank" onClick={() => window.location.href = 'https://julianehiem.dev/'}> <h3>Portfolio</h3> </div>
-      <div onClick={() => window.location.href = 'https://github.com/JulianEhiem'}> <h3>Github</h3> </div>
-    </div>
+    // <div className={styles.navContainer}>
+    //   <div onClick={() => router.push('/')}> <h3>Home</h3> </div>
+    //   <div target ="_blank" onClick={() => window.location.href = 'https://julianehiem.dev/'}> <h3>Portfolio</h3> </div>
+    //   <div onClick={() => window.location.href = 'https://github.com/JulianEhiem'}> <h3>Github</h3> </div>
+    // </div>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+            <Toolbar disableGutters>
+
+            </Toolbar>
+        </Container>
+      </AppBar>
   );
 }
