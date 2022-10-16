c08109923277eb2d4bf0909a75b9ab667cbdd3ca
JulianEhiem <jules.ehiem@gmail.com>
2022-10-16 07:15:22 -0400

    linting PostContext.js

--- components/PostContext.js
+++ components/PostContext.js
@@ -1,32 +1,4 @@
 import { createContext } from 'react';
 
+// eslint-disable-next-line import/prefer-default-export
 export const PostContext = createContext();
-
-// const PostContentProvider = ({posts}) => {
-
-//     const [mappedPosts, setMappedPosts] = useState([]);
-
-//     useEffect(() => {
-//         if(posts.length) {
-//             const imgBuilder = imageUrlBuilder({
-//                 projectId: 'nttcyj7x',
-//                 dataset: 'production',
-//             });
-
-//             setMappedPosts(
-//                 Post.map
-//             )
-//         }else {
-//             setMappedPosts([]);
-//         }
-
-//     }, [posts])
-
-//     return (
-//         <PostContext.Provider value ={{mappedPosts, setMappedPosts}}>
-//             {props.children}
-//         </PostContext.Provider>
-//     )
-// }
-
-// export default PostContentProvider;
